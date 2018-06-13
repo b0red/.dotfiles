532da6f946dfd0153bf5197920ac1dd905216e26
b0red <mr.akita@gmail.com>
2018-06-13 10:43:42 +0200

    addition

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -106,7 +106,7 @@ alias lll="ls -alFGH --color | less -R"
 # sleep 10; alert
 #alias alert ='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
-###     Replace top
+###     Replace top, du, df
 #
 alias top="htop"
 alias du="ncdu"
