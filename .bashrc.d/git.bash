a52d4ee11933b0abdcc6376ceba6f39a4d8319a2
b0red <mr.akita@gmail.com>
2019-01-07 12:42:52 +0100

    update in git aliases

--- .bashrc.d/git.bash
+++ .bashrc.d/git.bash
@@ -14,21 +14,35 @@ alias lg='git log --graph --full-history --all --color --pretty=format:"%x1b[31m
 
 ###	Git command shortcuts
 #
-alias {gstat,gst}='git status' 
-# Warning: gst conflicts with gnu-smalltalk (when used).
+alias g="git"
+alias gm="git merge"
+alias gmv="git mv"
+alias {gstat,gs}='git status' 
+alias gss="git status -s"
 alias {gbra,gb}='git branch'
+alias gc="git clone"
 alias {gco,go}='git checkout'
 alias {gcob,gob}='git checkout -b '
 alias {gadd,ga}='git add -A .'
 alias {gcom,gc}='git commit -m $1'
 alias {gpul,gl}='git pull '
-alias {gpus,gh}='git push '
+alias {gpus,gp}='git push '
 alias {gpullom,g_pull}='git pull origin master'
 alias {gpushom,g_push}='git push origin master'
-alias {gdiff,gd}="git status"
 alias gg='git grep '
 alias cdgit='cd "$(git rev-parse --show-toplevel 2> /dev/null)"'
 alias git-pull-all='find $(git rev-parse --show-toplevel 2> /dev/null) -name .git -type d -execdir git pull -v ";"'
+alias glum="git pull upstream master"
+alias gpr="git pull --rebase"
+alias gppd="git pull && git push origin develop"
+alias ggpm="git pull && git push origin master"
+alias gfrb="git fetch && git rebase"
+
+###     git functions
+#
+function gcp() {
+          git commit -am "$1" && git push origin master
+      }
 
 ###     Just checking
 #
