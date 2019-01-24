eee4f25627515cce02c5d03ab944d4cceddd56c4
b0red <mr.akita@gmail.com>
2019-01-24 19:13:42 +0100

    minor changes

--- .bashrc.d/aliases.bash
+++ .bashrc.d/aliases.bash
@@ -139,7 +139,7 @@ alias no_extensions='find . -type f ! -name "*.*"'
 alias {module-update,modup}="git submodule foreach git pull origin master"
 alias weather="curl wttr.in/stockholm"
 alias comstat="push \"Command ran! (uname -n)\" || push \"Command failed!\""
-if command_exist mc; then alias mc="sudo mc"; fi
+if command_exists mc; then alias mc="sudo mc"; fi
 
 ###     Kill all zombieprocesses
 #
