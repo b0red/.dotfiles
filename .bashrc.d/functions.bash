544b1722d1c83fd808e5afffd41865ca90c34a76
b0red <mr.akita@gmail.com>
2019-02-18 12:54:55 +0100

    Not working entry in functions.bash

--- .bashrc.d/functions.bash
+++ .bashrc.d/functions.bash
@@ -254,16 +254,14 @@ function command_exists() {
 }
 
 
-####    get active NIC
-#
 ###     Get active Network Interface
 #
-function ActiveNic() { 
+function getnic() { 
     Active_Nic=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
     echo $Active_Nic
 }
 
-### Function for simple search and replace in folder
+###     Function for simple search and replace in current folder
 #
 function SR() {
     echo -e "Search and replace for text in files: ${ORANGE} $PWD ${NC}\nSearch for:"
@@ -273,20 +271,59 @@ function SR() {
     find ./ -type f -exec sed -i 's/$string_1/$string_2/g' {} \;
 }
 
-### Function for renaming parts of or whole filnem
+###     Function for renaming parts of or whole filnem
 #
 function FR() {
     echo -e "Search and replace in filename in current ($PWD) folder\nSearch for:"
     read string_1
-    echo -e "Replace ${yellow}$string_1 with:"
+    echo -e "Replace ${yellow} $string_1 with:"
     read string_2
     find ./ -type -f exec rename 's/$string_1/$string_2/g' *
 }
 
-### Function for dotfind (find folders with space in name
+###     Function for dotfind (find folders with space in name
 #
 function dotfind(){
     find . -maxdepth 2 -type d -regex '.*/[^./][^/]*\.[^/]*'
+}
+
+function reverseempty() {
+echo -e "Choose what to search for, will delete folders ${ORANGE}NOT${NC} containing (music|movies|epub).\nEnter choice:"
+    source ~/bin/gits/bash-spinner/spinner.sh
+    while :
+    do read choice
+        case $choice in     
+            music)
+                #music
+                echo -e "searching for folders not containing ${GREEN} music files ${NC} in $pwd"
+                start_spinner 'searching...'
+                find . -depth -type d \! -exec sh -c 'find "$1" \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.ogg" -o -iname "*.wav" -o -iname "*.m4a" \) -type f | read a' _ {} \;-print  $1\; # -exec rm -rfv -- {} \;
+                stop_spinner $?
+                #exit 0
+                ;;
+            movies)
+                #movies
+                echo -e "searching for folders not containing ${GREEN} video files ${NC} in $pwd"
+                start_spinner 'searching...'
+                find . -depth -type d \! -exec sh -c 'find "$1" \( -iname "*.mov" -o -iname "*.avi" -o -iname "*.mkv" -o -iname "*.vob" -o -iname "*.ogg" -o -iname "*.wmv" -o -iname "*m4v" \) -type f | read a' _ {} \;-print \;# -exec rm -rfv -- {} \;
+                stop_spinner $?
+               # exit 0
+                ;;
+            epub)
+                #epub
+                echo -e "searching for folders not containing  ${GREEN} epubs ${choice} ${NC} in $pwd"
+                start_spinner 'searching...'
+                find . -depth -type d \! -exec sh -c 'find "$1" \( -iname "*.epub" -o -iname "*.azw" -o -iname "*.mobi" -o -iname "*.pdf" \) -type f | read a' _ {} \;-print \;# -exec rm -rfv -- {} \;
+                stop_spinner $?
+                #exit 0
+                ;;
+            *)
+                #else
+                echo -e "Nothing choosen!\n exiting!"
+                exit 0
+                ;;
+        esac
+    done
 }
 
 ### Function to backup latest commands
