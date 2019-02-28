79142c68bad7f77db65007aa26a6d170db45497a
b0red <mr.akita@gmail.com>
2019-02-28 20:11:29 +0100

    update

--- test.sh
+++ test.sh
@@ -8,42 +8,66 @@
 ##########################################################################################
 
 SHELL=/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-export TERM=${TERM:-dumb}
-export DISPLAY=:0.0
-
-
-APPARRAY=(curl htop ncdu pydf tree tmux vim mc)    
-
-function app_installer(){
-    ###     Install software (yes/no)
-    #
-    for APP in "${APPARRAY[@]}"
-    do
-        #echo $APP
-        if command -v $APP 2> /dev/null; then
-            echo "$APP already installed!" >> $LOG
-        elif ! [ -x command -v $APP 2>/dev/null ]; then
-           sudo apt install $APP
-            echo "Installed $APP" >> $LOG
-        else
-            echo "$APP FAILED TO INSTALL!!!" >> $LOG
-        fi
-    done 
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
 }
 
-app_installer; sleep 1
+OS=`lowercase \`uname\``
+KERNEL=`uname -r`
+MACH=`uname -m`
 
-# for APP in "${APPARRAY[@]}"
-#     do
-#         echo  ny loop
-#     ## create oneliner?
-#     if (command -v $APP 2>/dev/null); then
-#           echo $APP already installed 
-#     else
-#         echo will install ${APP}
-#         install $APP
+if [ "{$OS}" == "windowsnt" ]; then
+    OS=windows
+elif [ "{$OS}" == "darwin" ]; then
+    OS=mac
+else
+    OS=`uname`
+    if [ "${OS}" = "SunOS" ] ; then
+        OS=Solaris
+        ARCH=`uname -p`
+        OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+    elif [ "${OS}" = "AIX" ] ; then
+        OSSTR="${OS} `oslevel` (`oslevel -r`)"
+    elif [ "${OS}" = "Linux" ] ; then
+        if [ -f /etc/redhat-release ] ; then
+            DistroBasedOn='RedHat'
+            DIST=`cat /etc/redhat-release |sed s/\ release.*//`
+            PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/SuSE-release ] ; then
+            DistroBasedOn='SuSe'
+            PSUEDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
+            REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
+        elif [ -f /etc/mandrake-release ] ; then
+            DistroBasedOn='Mandrake'
+            PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+            REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+        elif [ -f /etc/debian_version ] ; then
+            DistroBasedOn='Debian'
+            DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
+            PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
+            REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
+        fi
+        if [ -f /etc/UnitedLinux-release ] ; then
+            DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+        fi
+        OS=`lowercase $OS`
+        DistroBasedOn=`lowercase $DistroBasedOn`
+        readonly OS
+        readonly DIST
+        readonly DistroBasedOn
+        readonly PSUEDONAME
+        readonly REV
+        readonly KERNEL
+        readonly MACH
+    fi
 
-#     sleep 1
-# fi
-#     done
+fi
+echo OS: $OS
+cat /etc/lsb-release
+echo -e "\nDIST: $DIST"
+echo "DistroBasedOn: $DistroBasedOn"
+echo "PSUEDONAME: $PSUEDONAME"
+echo "REV: $REV"
+echo "KERNEL: $KERNEL"
+echo "MACH: $MACH"
