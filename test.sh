868bf55991da9c80aaa6f58c466d90623782968b
b0red <mr.akita@gmail.com>
2019-03-05 17:08:02 +0100

    changed test.sh

--- test.sh
+++ test.sh
@@ -7,13 +7,71 @@
 ##
 ###############################################################################################
 
-debug=0
+debug=1
 
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
 
-grep -qxF "if [[ \$TMUX ]]; then source ~/.tmux-git/tmux-git.sh; fi" ~/.bashrc
-if [ $? -ne 0 ]; then
-  #sed -i "$ a port=9033" $light.conf
-  echo line is there
+OS=$(uname); OS="${OS,,}"
+KERNEL=$(uname -r)
+MACH=$(uname -m)
+
+if [ "${OS}" == "windowsnt" ]; then
+    OS=windows
+elif [ "${OS}" == "darwin" ]; then
+    OS=mac
 else
-    echo "port=9033 already added"
+    OS="linux"
+    if [ "${OS}" = "SunOS" ] ; then
+        OS=Solaris
+        ARCH=$(uname -p)
+        OSSTR="${OS} ${REV}(${ARCH} "uname -v")"
+    elif [ "${OS}" = "AIX" ] ; then
+        OSSTR="${OS} "oslevel" ("oslevel -r")"
+    elif [ "${OS}" = "linux" ] ; then
+        if [ -f /etc/redhat-release ] ; then
+            DistroBasedOn='RedHat'
+            DIST=$(cat /etc/redhat-release |sed s/\ release.*//)
+            PSEUDONAME=$(cat /etc/redhat-release | sed s/.*\(// | sed s/\)//)
+            REV=$(cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//)
+        elif [ -f /etc/SuSE-release ] ; then
+            DistroBasedOn='SuSe'
+            PSEUDONAME=$(cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//)
+            REV=$(cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //)
+        elif [ -f /etc/mandrake-release ] ; then
+            DistroBasedOn='Mandrake'
+            PSEUDONAME=$(cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//)
+            REV=$(cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//)
+        elif [ -f /etc/debian_version ] ; then
+            DistroBasedOn='Debian'
+            DIST=$(cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }')
+            PSEUDONAME=$(cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }')
+            REV=$(cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }')
+        fi
+        if [ -f /etc/UnitedLinux-release ] ; then
+            DIST=$(${DIST}["cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//"])
+        fi
+        OS="${OS,,}"
+        DistroBasedOn="${DistroBasedOn,,}"
+        readonly OS
+        readonly DIST
+        readonly DistroBasedOn
+        readonly PSEUDONAME
+        readonly REV
+        readonly KERNEL
+        readonly MACH
+    fi
+
+fi
+
+if [ $debug -eq 1 ]; then
+    echo "=============================="
+        echo OS:        ${OS^}
+        echo Distribution:      ${DIST^}
+        echo Distro based on:       ${DistroBasedOn^}
+        echo Pseudoname:        ${PSEUDONAME^}
+        echo Revision:       ${REV^}
+        echo Kernel:      ${KERNEL^}
+        echo Machine:      $MACH
 fi
