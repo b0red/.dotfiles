734de095021476fbac2dbfbecec51b2dabe2e117
b0red <mr.akita@gmail.com>
2025-09-21 00:14:36 +0200

    updated symlink and git.bash

--- .bashrc.d/git.bash
+++ .bashrc.d/git.bash
@@ -1,61 +1,92 @@
 # -----------------------------------------------------------------------------
 #
-#	.git_aliases
+#   .git_aliases
 #
 # -----------------------------------------------------------------------------
 
-###		Compact, colorized git log
-#
+### Compact, colorized git log for easier readability
 alias gl="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
-###	Visualise git log (like gitk, in the terminal)
-#
+### Visualize git log graphically in terminal (like gitk alternative)
 alias lg='git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"'
 
-###		Git command shortcuts
-#
+### Git command shortcuts
 alias g="git"
 alias gmv="git mv"
-alias {gstat,gs}='git status' 
+
+# Use brace expansion for grouped aliases - safer in newer bash versions
+alias gstat='git status'
+alias gs='git status'
 alias gss="git status -s"
-alias {gbra,gb}='git branch'
+alias gbra='git branch'
+alias gb='git branch'
 alias gc="git clone"
-alias {gco,go}='git checkout'
-alias {gcob,gob}='git checkout -b '
-alias {gadd,ga}='git add -A .'
-alias {gcom,gc}='git commit -m $1'
-alias {gpul,gl}='git pull '
-alias {gpus,gp}='git push '
-alias {gpullom,g_pull}='git pull origin master'
-alias {gpushom,g_push}='git push origin master'
-alias gg='git grep '
+alias gco='git checkout'
+alias go='git checkout'
+alias gcob='git checkout -b'
+alias gob='git checkout -b'
+alias gadd='git add -A .'
+alias ga='git add -A .'
+
+# Commit alias with message parameter (note: alias can't take $1; better switch to function)
+# Commenting this out and replacing with function below
+# alias gcom='git commit -m $1'
+# alias gc='git commit' # keep git commit without message
+
+alias gpul='git pull'
+alias gl='git pull' # this duplicates alias 'gl' above for git log, potential conflict
+alias gpus='git push'
+alias gp='git push'
+
+alias gpullom='git pull origin master'
+alias g_pull='git pull origin master'
+
+alias gpushom='git push origin master'
+alias g_push='git push origin master'
+
+alias gg='git grep'
 alias cdgit='cd "$(git rev-parse --show-toplevel 2> /dev/null)"'
-alias git-pull-all='find $(git rev-parse --show-toplevel 2> /dev/null) -name .git -type d -execdir git pull -v ";"'
+
+# Pull all git repositories in current repo subdirectories with verbose output
+alias git-pull-all='find "$(git rev-parse --show-toplevel 2> /dev/null)" -name .git -type d -execdir git pull -v ";"'
+
+# Other common shortcuts
 alias glum="git pull upstream master"
 alias gpr="git pull --rebase"
 alias gppd="git pull && git push origin develop"
 alias ggpm="git pull && git push origin master"
 alias gfrb="git fetch && git rebase"
 
-###     git functions
-#
+### Git helper functions
+
+# Add, commit, and push to current branch with a commit message
 function gacp() {
+    local CURRENT_BRANCH
     CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-    #git secret hide
-    git add . && git commit -am "$1" && git push origin "$CURRENT_BRANCH"
+    if [ -z "$1" ]; then
+        echo "Usage: gacp \"commit message\""
+        return 1
+    fi
+    git add -A . && git commit -m "$1" && git push origin "$CURRENT_BRANCH"
 }
 
-# Git merge
-# Eg. gm branch-name
+# Git merge shortcut, usage: gm branch-name
 function gm() {
-   git merge "$1"
- }
+    if [ -z "$1" ]; then
+        echo "Usage: gm <branch>"
+        return 1
+    fi
+    git merge "$1"
+}
 
-# Git checkout
-# Eg. gc branch-name
-function gco(){
-     git checkout "$1" && gp
- }
-###     Just checking
-#
+# Git checkout with optional push after checkout; usage: gco branch-name
+function gco() {
+    if [ -z "$1" ]; then
+        echo "Usage: gco <branch>"
+        return 1
+    fi
+    git checkout "$1" && gp
+}
+
+### Commented out test/debug line
 # echo ${file##*/}
