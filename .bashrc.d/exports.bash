4a0f81dbcfa06b60f4d8764e7aa342ac7d8dbb55
b0red <mr.akita@gmail.com>
2026-01-19 04:37:47 +0100

    latest

--- .bashrc.d/exports.bash
+++ .bashrc.d/exports.bash
@@ -50,4 +50,11 @@
 # [PASTE ALL YOUR REMAINING ORIGINAL EXPORTS HERE with [ -z ] guard]
 # Example: [ -z "${MY_CUSTOM_VAR:-}" ] && export MY_CUSTOM_VAR='value'
 
+# https://github.com/raylee/tldr-sh-client
+export TLDR_HEADER='magenta bold underline'
+export TLDR_QUOTE='italic'
+export TLDR_DESCRIPTION='green'
+export TLDR_CODE='red'
+export TLDR_PARAM='blue'
+
 # End - no deletions, fully idempotent
